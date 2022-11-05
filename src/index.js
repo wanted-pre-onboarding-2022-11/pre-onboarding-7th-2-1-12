2892190fdb036b94f6639941e2f54a8b5183abcd
devyouth94 <devyouth94@gmail.com>
2022-11-06 02:54:35 +0900

    Feat: Index 파일 수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+import { store } from "app/config/store";
+import { Provider } from "react-redux";
+
+import dayjs from "dayjs";
+import isLeapYear from "dayjs/plugin/isLeapYear"; // 윤년 판단 플러그인
+import "dayjs/locale/ko"; // 한국어 가져오기
+
+import { theme } from "styles/theme";
+import GlobalStyles from "styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
+dayjs.extend(isLeapYear); // 플러그인 등록
+dayjs.locale("ko"); // 언어 등록
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
+);
