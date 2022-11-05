afe080041e938c6d7a922b7805881107bd83c93b
yong <tkddbs1644@gmail.com>
2022-11-05 16:04:59 +0900

    feat: BoardCard 컴포넌트 구현

---
+++ src/components/common/BoardCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const BoardCard = () => {
+  return <Container>BoardCard</Container>;
+};
+
+export default BoardCard;
+
+const Container = styled.section`
+  background-color: white;
+  box-sizing: border-box;
+  padding: 24px;
+  overflow: hidden;
+  border-radius: 16px;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+`;
