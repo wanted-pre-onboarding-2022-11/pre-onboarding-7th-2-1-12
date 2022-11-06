390f3779098aa251417f96a4e2ea4592d79865b1
Hemudi <ksum1205@naver.com>
2022-11-06 16:16:20 +0900

    Feat: body background 색상 수정

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,10 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { COLORS } from "styles/theme";
 
 export default createGlobalStyle`
     ${reset}
 
     html {
         font-size: 62.5%;
-    }  
+    } 
+
+    body {
+        background: ${COLORS.LIGHT_GRAY};
+    }
 `;
