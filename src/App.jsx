ee1f95b40cc18552234b6d014051dbcee6c2a6d2
Hemudi <ksum1205@naver.com>
2022-11-06 14:53:31 +0900

    Feat: GlobalStyle 및 theme 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import GlobalStyle from "styles/GlobalStyle";
+import Router from "routes";
+
 function App() {
   return (
     <>
-        <Router />
+      <GlobalStyle />
+      <Router />
     </>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,30 @@
+const COLORS = {
+  BLACK: "#3A474E",
+  WHITE: "#FFFFFF",
+  BLUE: "#586CF5",
+  GRAY: "#94A2AD",
+  BORDER: "#D1D8DC",
+  BUTTON: "#EDEFF1",
+  LIGHT_GRAY: "#F6F7F8",
+};
+
+const FONT_SIZE = {
+  X_LARGE: "2.6rem",
+  LARGE: "1.6rem",
+  MEDIUM: "1.4rem",
+  SMALL: "1.2rem",
+};
+
+const FONT_WEIGHT = {
+  BOLD: "900",
+  MEDIUM: "700",
+  REGULAR: "500",
+  LIGHT: "100",
+};
+
+const FONT = {
+  SIZE: FONT_SIZE,
+  WEIGHT: FONT_WEIGHT,
+};
+
+export { COLORS, FONT };
