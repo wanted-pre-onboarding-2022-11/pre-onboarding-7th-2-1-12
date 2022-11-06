f93940cde60661f3e6603cafbd7e9b23310bf507
chaechae <dokailove6326@gmail.com>
2022-11-06 13:43:58 +0900

    Chore: utils 함수 작동되는 지 확인

--- src/Pages/AdPage/index.jsx
+++ src/Pages/AdPage/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 // import { useGetData } from "../../hooks/useGetData";
-// import { ListImp } from "../../utils/adList";
+// import { ListImp } from "../../utils/listImp";
 
 const AdPage = () => {
-  // const list = useGetData();
+  // const list = useGetData(true);
   // const sortData = new ListImp(list?.payload?.ads);
   // console.log(sortData.sorted("status", null));
   return <div>AdPage</div>;
--- src/Pages/DashboardPage/index.jsx
+++ src/Pages/DashboardPage/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "react-day-picker/dist/style.css";
+import { DayPicker } from "react-day-picker";
+import { useState } from "react";
+// import { WeekImp } from "../../utils/weeklmp";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const [selectedDay, setSelectedDay] = useState({});
+  // const weekImp = new WeekImp();
+  // console.log(weekImp.weekCal(selectedDay));
+  return (
+    <>
+      <DayPicker
+        mode="range"
+        min={7}
+        max={7}
+        selected={selectedDay}
+        onSelect={setSelectedDay}
+        fromMonth={new Date(2022, 1)}
+        toDate={new Date(2022, 3, 20)}
+      />
+      <div>DashboardPage</div>;
+    </>
+  );
 };
 
 export default DashboardPage;
