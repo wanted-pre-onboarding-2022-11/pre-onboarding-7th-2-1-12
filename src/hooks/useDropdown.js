bde8f33e0f43c437b47624aec12e7ac16a882cbb
devyouth94 <devyouth94@gmail.com>
2022-11-06 12:47:46 +0900

    Feat: 드랍다운 훅스 생성

---
+++ src/hooks/useDropdown.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+const useDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropRef = useRef(null);
+
+  const handleClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const onClick = ({ target }) => {
+      if (dropRef.current && !dropRef.current.contains(target)) {
+        setIsOpen((prev) => !prev);
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener("click", onClick);
+    }
+
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, [isOpen]);
+
+  return [isOpen, dropRef, handleClick];
+};
+
+export default useDropdown;
