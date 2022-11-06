7170630da970738aa87c2f837835668f5d007d64
Hemudi <ksum1205@naver.com>
2022-11-06 15:48:09 +0900

    Feat: Icon 컴포넌트 사이즈, 색상 변경 기능 추가

--- src/components/common/Icon/index.jsx
+++ src/components/common/Icon/index.jsx
@@ -1,8 +1,21 @@
 import Icons from "components/common/Icon/Icons";
+import styled from "styled-components";
+import { COLORS } from "styles/theme";
 
-const Icon = ({ iconName }) => {
-  const IconComponent = Icons[iconName];
+const Icon = ({ type, width, height, color }) => {
+  const IconComponent = createStyledIcon(type, { width, height, color });
   return <IconComponent />;
+};
+
+const createStyledIcon = (type, { width, height, color }) => {
+  const StyledIcon = styled(Icons[type])`
+    width: ${width || "24px"};
+    height: ${height || "24px"};
+    path {
+      fill: ${color?.path || COLORS.BLACK};
+    }
+  `;
+  return StyledIcon;
 };
 
 export default Icon;
