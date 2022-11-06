b16a13a9931865a9f8a03950f23e60b21b4c2c1f
chaechae <dokailove6326@gmail.com>
2022-11-06 14:49:58 +0900

    Feat: 대시보드에 필요한 utils 구현

--- src/utils/dashboard.js
+++ src/utils/dashboard.js
@@ -1,6 +1,34 @@
 /*
-1. 날짜 선택
-2. 날짜 sort
-3. 각자 데이터 합친
-4. 각자 데이터만 Array
+// 1. 날짜 선택
+1. 날짜 sort
+2. 각자 데이터 합친
+3. 각자 데이터만 Array
 */
+
+export class DashboardImp {
+  #data;
+  constructor(data) {
+    this.#data = data;
+  }
+  weeksData(weeks) {
+    if (!weeks) {
+      return;
+    }
+    return this.#data.filter((item) => weeks.some((week) => week === item.date));
+  }
+  added(data, type) {
+    if (!data) {
+      return;
+    }
+    return data.reduce((prev, cur) => prev + cur[type], 0);
+  }
+  getList(data, type) {
+    if (!data) {
+      return;
+    }
+    return data.reduce((prev, cur) => {
+      prev.push(cur[type]);
+      return prev;
+    }, []);
+  }
+}
